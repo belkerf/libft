0642b2739e0cfbf01ca5b0f9dea6757ace250734
belkerf <belkerf@hotmail.com>
2024-09-14 18:23:28 +0100

    on it+

--- libft.h
+++ libft.h
@@ -20,6 +20,7 @@
 # include <stddef.h>
 # include <stdlib.h>
 
+void    ft_putnbr_fd(int n, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void    ft_putchar_fd(char c, int fd);
