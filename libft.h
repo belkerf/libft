31904168bb7b3d0189a8724452dcdb38febe2b1e
belkerf <belkerf@hotmail.com>
2024-09-16 19:18:10 +0100

    adding itoa

--- libft.h
+++ libft.h
@@ -20,6 +20,7 @@
 # include <stddef.h>
 # include <stdlib.h>
 
+char    *ft_itoa(int n);
 char    **ft_split(char const *s, char c);
 char    *ft_strtrim(char const *s1, char const *set);
 char    *ft_strjoin(char const *s1, char const *s2);
