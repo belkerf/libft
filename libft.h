b4da9fafd84ed69d933a74c4bb4ee525e88a0ab0
belkerf <belkerf@hotmail.com>
2024-09-17 22:48:25 +0100

    i_realy_doing_it

--- libft.h
+++ libft.h
@@ -26,6 +26,7 @@ void *content;
 struct s_list *next;
 } t_list;
 
+void    ft_lstclear(t_list **lst, void (*del)(void *));
 void    ft_lstdelone(t_list *lst, void (*del)(void *));
 void    ft_lstadd_back(t_list **lst, t_list *new);
 t_list  *ft_lstlast(t_list *lst);
