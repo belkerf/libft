c9ad78f942972e71008f48e88c20aae74122601a
belkerf <jbelkerf@gmail.com>
2024-10-22 14:58:02 +0100

    done

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,36 +6,54 @@
 /*   By: jbelkerf <jbelkerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:09:20 by jbelkerf          #+#    #+#             */
-/*   Updated: 2024/10/22 12:08:49 by jbelkerf         ###   ########.fr       */
+/*   Updated: 2024/10/22 14:57:48 by jbelkerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+int	check_overl(char *d, char *s, size_t n)
 {
-	char	buf[n + 1];
+	if (s < d && s + n > d)
+		return (1);
+	if (d < s && d + n > s)
+		return (2);
+	return (0);
+}
+
+void	*r_cpy(void *dest, const void *src, size_t n)
+{
+	char	*b;
 	char	*s;
-	char	*d;
 	size_t	i;
 
+	b = (char *)dest;
+	s = (char *)src;
+	if (n == 0)
+		return (dest);
+	if (b == NULL && s == NULL)
+		return (NULL);
 	i = 0;
+	while (n)
+	{
+		n--;
+		b[n] = s[n];
+	}
+	return (dest);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*s;
+	char	*d;
+	int		i;
+
 	s = (char *)src;
 	d = (char *)dest;
 	if (d == 0 && s == 0)
 		return (0);
-	i = 0;
-	while (i < n)
-	{
-		buf[i] = s[i];
-		i++;
-	}
-	buf[i] = 0;
-	i = 0;
-	while (i < n)
-	{
-		d[i] = buf[i];
-		i++;
-	}
-	return (dest);
+	i = check_overl(d, s, n);
+	if (i == 0 || i == 2)
+		return (ft_memcpy(dest, src, n));
+	return (r_cpy(dest, src, n));
 }
